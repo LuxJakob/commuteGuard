4b12ace36179de80005d6fc662145d3a1ade2869
LuxJakob <lux.jakob@gmail.com>
2025-06-06 17:53:42 +0200

    morning commute test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from datetime import datetime
+from datetime import datetime, time
 
 from src.send_email import send_email
 
 if __name__ == "__main__":
-    today = datetime.today()
+    # today = datetime.today()
+    today = datetime.combine(datetime.today().date(), time(7, 0))
+    # today = datetime.combine(datetime.today().date(), time(16, 0))
+
     send_email(today)
